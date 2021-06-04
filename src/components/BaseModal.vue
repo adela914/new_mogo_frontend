a8c135b3248c3422c9cd63cd8977f789788ac9f8
Kyunghwa <kyunghwa.oh@solytic.com>
2021-06-04 10:19:53 +0200

    enhance accessibility in modal

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -3,25 +3,24 @@
     <div class="modal-mask" :aria-label="label" role="dialog" aria-modal="true">
       <div class="modal-wrapper">
         <div class="modal-container">
-          <div class="modal-header">
+          <header class="modal-header">
             <slot name="header"> default header </slot>
             <button
               @click="$emit('close')"
               ref="closeButton"
+              aria-label="Close modal"
               class="closeBtn"
               @keydown.esc="$emit('close')"
             >
               <BaseIcon name="fas fa-times" />
             </button>
-          </div>
-
-          <div class="modal-body">
+          </header>
+          <section class="modal-body">
             <slot name="body"> default body </slot>
-          </div>
-
-          <div class="modal-footer">
+          </section>
+          <footer class="modal-footer">
             <slot name="footer"> default footer </slot>
-          </div>
+          </footer>
         </div>
       </div>
     </div>
